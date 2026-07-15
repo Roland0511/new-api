2be9f1ac3c10cf75b099e6337af5334eecce994f
lizhou.zhu <lizhou.zhu@happyelements.com>
2026-07-15 14:07:34 +0800

    fix(model): 避免令牌模型列表查询空用户组

--- controller/model.go
+++ controller/model.go
@@ -178,6 +178,9 @@ type modelListGroups struct {
 func getModelListGroups(c *gin.Context) (modelListGroups, error) {
 	tokenGroup := common.GetContextKeyString(c, constant.ContextKeyTokenGroup)
 	userGroup := common.GetContextKeyString(c, constant.ContextKeyUserGroup)
+	if userGroup == "" && tokenGroup == "" && common.GetContextKeyBool(c, constant.ContextKeyTokenModelLimitEnabled) {
+		return modelListGroups{}, nil
+	}
 	if userGroup == "" && (tokenGroup == "" || tokenGroup == "auto") {
 		var err error
 		userGroup, err = model.GetUserGroup(c.GetInt("id"), false)
